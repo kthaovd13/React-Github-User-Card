2e4462c9f49ab75894259993627b9aa31b0d02f2
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-07-14 22:13:01 -0500

    met MVP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import './App.css';
+import UserCard from './UserCard';
+import OtherCard from './OtherCard';
 
 class App extends React.Component {
   constructor() {
     super();
       this.state = {
+        // data: {},
         user: [],
         followers: []
       }
   }
 
+
+  // componentWillMount () {
+  //   fetch(`https://api.github.com/users/${this.state.user}/followers`)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //         console.log(response.data);
+  //         this.setState({ followers: response })})
+  //       .catch(err => console.log('errors in API request'));
+  // }
+
   componentDidMount() {
       console.log("mount working")
-      fetch('https://api.github.com/users/kthaovd13')
+      fetch(`https://api.github.com/users/kthaovd13`)
         .then(response => response.json())
         .then(response => {
-          console.log(response.data);
-          this.setState({ user: response.data })})
+          console.log(response);
+          this.setState({ user: response })})
         .catch(err => console.log('errors in API request'));
   
-      fetch('https://api.github.com/users/kthaovd13/followers')
+
+      fetch(`https://api.github.com/users/kthaovd13/followers`)
         .then(response => response.json())
-        .then(response => this.setState({ followers: response.data}))
+        .then(response => this.setState({ followers: response }))
         .catch(err => console.log('errors in API followers request'));
   }
 
@@ -29,6 +43,8 @@ class App extends React.Component {
       return (
         <div className="App">
           <h1>Github Card Followers</h1>
+          <UserCard user={this.state.user} />
+          <OtherCard followers={this.state.followers} />
         </div>
         );
     }
